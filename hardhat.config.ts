37a9e5d77f3d13b4f7bfa4c22cc0496c1a4cc243
danialmarat <doni.danialmarat20066@gmail.com>
2026-05-17 20:27:20 +0500

    chore(config): add localhost network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,10 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 31337
     },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    },
     baseSepolia: {
       url: process.env.BASE_SEPOLIA_RPC_URL || "",
       accounts
